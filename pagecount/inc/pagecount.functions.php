c76ac3264048b034525b73f22559040f6751234f
Quasiocculti <q@sed.by>
2023-09-14 21:41:40 +0300

    Apply coding guidelines

--- pagecount/inc/pagecount.functions.php
+++ pagecount/inc/pagecount.functions.php
@@ -8,23 +8,19 @@
 
 defined('COT_CODE') or die('Wrong URL');
 
-function cot_pagecount ($condition = '', $mode = '', $cats = '', $subs = true, $decl = 'pages') {
-
-	global $Ls;
+function sedby_pagecount($extra = '', $mode = '', $cats = '', $subs = true, $decl = 'pages') {
   require_once cot_langfile('page', 'module');
   require_once cot_incfile('pagelist', 'plug');
 
-  $where_cat = cot_compilecats($mode, $cats, (bool)$subs);
-	$condition = (empty($condition)) ? '' : 'AND '.$condition;
+  $sql_cats = (empty($mode)) ? "" : sedby_compilecats($mode, $cats, (bool)$subs);
+	$sql_extra = (empty($extra)) ? "" : $extra;
 
-	$db_pages = cot::$db->pages;
-	$totalitems = cot::$db->query("
-		SELECT COUNT(*)
-		FROM $db_pages
-		WHERE page_state='0' $where_cat $condition
-	")->fetchColumn();
+  $sql_cond = sedby_twocond($sql_cats, $sql_extra);
 
-	$totalitems = (empty($decl)) ? $totalitems : cot_declension($totalitems, $Ls[$decl]);
+	$db_pages = Cot::$db->pages;
+	$totalitems = Cot::$db->query("SELECT COUNT(*) FROM $db_pages $sql_cond")->fetchColumn();
+
+	$totalitems = (empty($decl)) ? $totalitems : cot_declension($totalitems, $decl);
 
 	return $totalitems;
 }
